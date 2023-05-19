d8fc7abb97d52209c498a2c4d6231271ac8af4cb
sageerkv <sageerkv93@gmail.com>
2023-05-19 14:33:13 +0530

    final commit

--- employee_project/employee_app/urls.py
+++ employee_project/employee_app/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('<int:id>/', views.employee_form  , name='employee_update'),
     path('delete/<int:id>/', views.employee_delete, name='employee_delete'),
     path('list/', views.employee_list, name='employee_list'),
-]
+]   
